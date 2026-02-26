0d81dc4ded6fb87f469d9765d36d39955c5c1040
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-02-26 14:47:29 +0530

    attention

--- multimodal_vlm/SigLIP.py
+++ multimodal_vlm/SigLIP.py
@@ -54,6 +54,46 @@ class SiglipVisionEmbedding(nn.Module):
         embed = embed.transpose(1,2)
         embed = embed + self.position_embed(self.position_ids)
         return embed
+
+class SiglipAttention(nn.Module):
+    def __init__(self, config: SiglipConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.embed_dim // self.num_heads
+        self.scale = self.head_dim**0.5
+        self.dropout = config.attention_dropout
+        
+        self.q_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.k_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.v_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        
+    def forward(self, hidden_state: torch.Tensor) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        batch_size, seq_len, _ = hidden_state.size()
+        query_states = self.q_proj(hidden_state)
+        key_states = self.k_proj(hidden_state)
+        value_states = self.v_proj(hidden_state)
+        #basically going from ([batch_size, num_patches, embed_dim]) to ([batch_size, num_heads, num_patches, head_dim])
+        query_states = query_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1,2)
+        key_states = key_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1,2)
+        value_states = value_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1,2)
+        
+        attn_weights = (torch.matmul(query_states, key_states.transpose(2,3))* self.scale)
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float).to(query_states.dtype)
+        attn_weights = nn.functional.dropout(attn_weights, p=self.dropout, training=self.training) # not really required, it isnt in paper but useful during training for overfitting
+        attn_output = torch.matmul(attn_weights, value_states)
+        
+        # we go from ([batch_size, num_heads, num_patches, head_dim]) to ([batch_size, num_patches, num_heads, head_dim])
+        attn_output = attn_output.transpose(1,2).contiguous()
+        # then from ([batch_size, num_patches, num_heads, head_dim]) to ([batch_size, num_patches, embed_dim])
+        attn_output = attn_output.reshape(batch_size, seq_len, self.embed_dim)
+        attn_output = self.out_proj(attn_output)
+        
+        return attn_output, attn_weights
+        
+        'so we started with (4, 1024) and ended with (4, 1024) dim'
     
 class SiglipMLP(nn.Module):
     def __init__(self, config: SiglipConfig):
